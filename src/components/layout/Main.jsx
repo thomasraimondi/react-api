497361c7f64066faaf72d25dbcdc3ba52f282430
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-28 15:32:01 +0200

    fix actors and actresses list

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 
 export default function Main() {
   const [actors, setActors] = useState([]);
@@ -12,43 +12,76 @@ export default function Main() {
     axios
       .get("https://lanciweb.github.io/demo/api/actors/")
       .then((response) => {
-        setActors(response.data);
+        const res = response.data;
+        setActors(
+          res.map(
+            ({
+              id,
+              image,
+              name,
+              birth_year,
+              nationality,
+              biography,
+              awards,
+            }) => ({
+              id: "actor-" + id,
+              image,
+              name,
+              birth_year,
+              nationality,
+              biography,
+              awards,
+            })
+          )
+        );
       })
       .then(() => {
         axios
           .get("https://lanciweb.github.io/demo/api/actresses/")
           .then((response) => {
-            setActresses(response.data);
+            const res = response.data;
+            setActresses(
+              res.map(
+                ({
+                  id,
+                  image,
+                  name,
+                  birth_year,
+                  nationality,
+                  biography,
+                  awards,
+                }) => ({
+                  id: "actress-" + id,
+                  image,
+                  name,
+                  birth_year,
+                  nationality,
+                  biography,
+                  awards,
+                })
+              )
+            );
           });
       });
-    setNewActors([...actors, ...actresses]);
     setIsLoading(false);
   };
 
-  //   const getActresses = () => {
-  //     setIsLoading(true);
-  //     axios
-  //       .get("https://lanciweb.github.io/demo/api/actresses/")
-  //       .then((response) => {
-  //         setActresses(response.data);
-  //       });
-  //   };
+  useEffect(() => {
+    setNewActors([...actors, ...actresses]);
+  }, [actors, actresses]);
 
   useEffect(() => {
     getActors();
     setIsLoading(false);
   }, []);
 
-  //   useEffect(() => {
-  //     getActresses();
-  //     setIsLoading(false);
-  //   }, []);
-
   return (
     <>
       <div className="container mx-auto flex-grow-1 border-2 border-b-black rounded-lg my-4">
         <div className="flex flex-col">
-          <h1 className="text-2xl font-bold p-4 border-b-2">Lista Attrici</h1>
+          <h1 className="text-2xl font-bold p-4 border-b-2">
+            Lista Attori e Attrici
+          </h1>
         </div>
         {isLoading ? (
           <div className="flex justify-center items-center h-full">
@@ -95,55 +128,6 @@ export default function Main() {
           </div>
         )}
       </div>
-      {/* <div className="container mx-auto flex-grow-1 border-2 border-b-black rounded-lg my-4">
-        <div className="flex flex-col">
-          <h1 className="text-2xl font-bold p-4 border-b-2">Lista Attori</h1>
-        </div>
-        {isLoading ? (
-          <div className="flex justify-center items-center h-full">
-            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
-            <p>Loading...</p>
-          </div>
-        ) : (
-          <div className="flex gap-4 flex-wrap justify-center p-4">
-            {actors.map((actor) => (
-              <div
-                className="card border-2 border-gray-300 rounded-lg p-4 w-1/4"
-                key={actor.id}
-              >
-                <div className="card-image">
-                  <img
-                    className="w-full h-64 object-contain"
-                    src={actor.image}
-                    alt={actor.name}
-                  />
-                </div>
-                <div className="card-content">
-                  <h2 className="text-xl font-bold my-2">{actor.name}</h2>
-                  <div className="flex flex-col">
-                    <span className="font-bold">
-                      Bith Year:{" "}
-                      <span className="font-normal">{actor.birth_year}</span>
-                    </span>
-                    <span className="font-bold">
-                      Nationality:{" "}
-                      <span className="font-normal">{actor.nationality}</span>
-                    </span>
-                    <span className="font-bold">
-                      Biography:{" "}
-                      <span className="font-normal">{actor.biography}</span>
-                    </span>
-                    <span className="font-bold">
-                      Awards:{" "}
-                      <span className="font-normal">{actor.awards}</span>
-                    </span>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        )}
-      </div> */}
     </>
   );
 }
